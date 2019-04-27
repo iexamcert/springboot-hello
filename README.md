1baf49b3d4d407fe4e01f32ea480524ae9709a26
king <king@192.168.0.104>
2019-04-28 00:59:58 +0800

    auto_deploy

--- README.md
+++ README.md
@@ -1,14 +1,15 @@
 ###DEV
 ---  
-requirement:  
-1. 开发机器上已经安装docker。  
+为了方便使用hub.docker.com来存放Artifacts. 实际生产中可以使用docker registry或者harbor来管理Artifacts。
+
 
 STEPS：  
-1. 开发人员拉去代码、修改之后保存。  
-2. 在代码根目录运行```/bin/bash start.sh```  
+1.  在代码根目录运行```/bin/bash start.sh```  
+
 
 ###CICD  
 ---
+<del>
    因为在整个CI/CD的过程中，涉及到jenkins slave的动态注册、使用全局工具tool编译打包jar文件、使用credentials推送镜像、使用kubectl工具操作k8s集群等操作。所以本案例提供了一个minikube的k8s环境和安装在k8s之上的jenkins。  
 
 |项目名：|路径：|用户名：|密码|  
@@ -17,3 +18,4 @@ STEPS：
 |项目访问地址：|http://149.28.134.208:30003/hello |||
 |github:|https://github.com/iexamcert/springboot-hello.git |||    
 |Dockerhub:|iexamcert/ springboot-hello | | |  
+ </del>
--- start.sh
+++ start.sh
@@ -1,59 +1,31 @@
 #!/bin/bash
-POM_FILE="pom.xml"
-IMAGE_VERSION="V1"
-DOCKER_PORT=""
-
-#还原Dockerfile模板
-cp Dockerfile.bak Dockerfile
-#DOCKER_HOST="127.0.0.1"
-###根据pom文件生成jar包的名称
-function get_project_name() {
-	JAR_BASE_NAME=`grep artifactId ${POM_FILE}|awk -F ">" '{print $2}' | awk -F "<" '{print $1}'|head -1`
-	VERSION=`grep version ${POM_FILE}|awk -F ">" '{print $2}' | awk -F "<" '{print $1}'|sed -n '2p'`
-	JAR_NAME="${JAR_BASE_NAME}"-"${VERSION}".jar
-	DOCKER_PORT=""
-
+set -ex
+#判断系统为Centos或者Ubuntu
+function SYS_OS() {
+  SYSTEM_OS=`lsb_release -i|awk -F ' ' '{print $NF}'`
 }
 
-###修改Dockerfile里的jar包名称
-function change_name() {
-	sed -i "" "s/JAR_NAME/${JAR_NAME}/g" Dockerfile
+#安装ansible and boto
+function INSTALL_ANSIBLE() {
+  #statements
+  if [[ ${SYSTEM_OS} == "Centos" ]];then
+    yum install -y ansible && pip install boto
+  elif [[ ${SYSTEM_OS} == "Ubuntu" ]]; then
+    apt install -y ansible && pip install boto
+  fi
 }
 
-###buid docker image
-function docker_build() {
-	#docker build -t ${DOCKER_HOST}/${JAR_BASE_NAME}:${DOCKER_VERSION} .
-	echo "-------------docker build-----------------"
-	docker build -t ${JAR_BASE_NAME}:${IMAGE_VERSION} .
+#获取账号信息
+function GET_AWS_SECRET() {
+  #statements
+  read -p "Enter your AWS_ACCESS_KEY_ID:" AWS_ACCESS_KEY_ID
+  read -p "Enter your AWS_SECRET_ACCESS_KEY:" AWS_SECRET_ACCESS_KEY
 }
 
-###docker push
-function docker_push() {
-	docker push ${DOCKER_HOST}/${JAR_BASE_NAME}:${IMAGE_VERSION}
+function CREATE_EC2_INSTANCE(parameter) {
+  #statements
 }
 
-###docker run
-function docker_run() {
-	echo "-------------docker run -----------------"
-	docker run -d --name myproject ${JAR_BASE_NAME}:${IMAGE_VERSION}
+function INIT_EC2(parameter) {
+  #statements
 }
-
-###docker log and clean
-function log_and_clean() {
-	echo "运行docker logs --tail -f myproject 查看日志  "
-	echo "docker logs --help 获取更多帮助"
-	echo "docker rm -f myproject 删除此容器"
-}
-
-###maven mvn_package
-function mvn_package() {
-	docker run -it --rm --name my-maven-project -v "$(pwd)":/usr/src/mymaven -w /usr/src/mymaven maven:3.3-jdk-8 mvn clean install
-	#statements
-}
-
-mvn_package
-get_project_name
-change_name
-docker_build
-docker_run
-log_and_clean
